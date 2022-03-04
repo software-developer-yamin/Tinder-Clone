d9a95b7dfcf03e5bf428b834ce5af3b842b47b79
DeveloperYamin <developer.yamin@gmail.com>
2022-03-05 02:50:04 +0600

    add some data AuthModal component

--- src/components/AuthModal.jsx
+++ src/components/AuthModal.jsx
@@ -1,12 +1,27 @@
+import { useForm } from "react-hook-form";
+
 function AuthModal({ setShowModal }) {
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm();
+  const onSubmit = (data) => console.log(data);
+
   const handleClick = () => {
     setShowModal(false);
   };
 
+  const isSignUp = true;
+
   return (
-    <div className="auth-modal" >
-      <button onClick={handleClick}>Close</button>
-      Auth Modal
+    <div className="auth-modal">
+      <div className="close-icon" onClick={handleClick}>
+        <button>Close</button>
+      </div>
+      <h2>{isSignUp ? "CREATE ACCOUNT" : "LOG IN"}</h2>
+      <p>By clicking log in, you agree to our terms. Learn how we process your data in our Privacy Policy.</p>
     </div>
   );
 }
