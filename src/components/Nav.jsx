915e76d0d4358b80461d15ca3ec4a9cc5610554c
DeveloperYamin <developer.yamin@gmail.com>
2022-03-04 19:30:36 +0600

    add parameter in nav component

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,5 +1,19 @@
-function Nav() {
-  return <div>Nav</div>;
+import colorLogo from "../images/tinder_color_logo.png";
+import whiteLogo from "../images/tinder_white_logo.png";
+
+function Nav({ minimal,authToken }) {
+  return (
+    <nav>
+      <div className="logo_container">
+        <img
+          className="logo"
+          src={minimal ? colorLogo : whiteLogo}
+          alt="Tinder_Logo"
+        />
+      </div>
+      {!authToken && <button className="nav-button" >Log In</button>}
+    </nav>
+  );
 }
 
 export default Nav;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,13 @@
 import Nav from "../components/Nav";
 
 function Home() {
-  const authToken = true;
+  const authToken = false;
   const handleClick = () => {
     console.log("clicked");
   };
   return (
     <>
-      <Nav />
+      <Nav minimal={false} authToken={authToken} />
       <div className="home">
         <h1>Swipe Right@</h1>
         <button className="primary-button" onClick={handleClick}>
