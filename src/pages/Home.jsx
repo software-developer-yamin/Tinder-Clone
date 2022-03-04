bdf1b6863016ca9450958ea81e3f0bbabe380536
DeveloperYamin <developer.yamin@gmail.com>
2022-03-04 14:36:59 +0600

    customise home component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,21 @@
+import Nav from "../components/Nav";
+
 function Home() {
-     return <div>
-       
-  </div>;
+  const authToken = true;
+  const handleClick = () => {
+    console.log("clicked");
+  };
+  return (
+    <>
+      <Nav />
+      <div className="home">
+        <h1>Swipe Right@</h1>
+        <button className="primary-button" onClick={handleClick}>
+          {authToken ? "Sign Out" : "Create Account"}
+        </button>
+      </div>
+    </>
+  );
 }
 
 export default Home;
