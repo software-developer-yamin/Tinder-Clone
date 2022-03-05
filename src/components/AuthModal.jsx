864026fe3c2f48e39c8f998f6870aa7cc7f56652
DeveloperYamin <developer.yamin@gmail.com>
2022-03-05 13:30:28 +0600

    create sign up form useing react-hook-form

--- src/components/AuthModal.jsx
+++ src/components/AuthModal.jsx
@@ -21,7 +21,37 @@ function AuthModal({ setShowModal }) {
         <button>Close</button>
       </div>
       <h2>{isSignUp ? "CREATE ACCOUNT" : "LOG IN"}</h2>
-      <p>By clicking log in, you agree to our terms. Learn how we process your data in our Privacy Policy.</p>
+      <p>
+        By clicking log in, you agree to our terms. Learn how we process your
+        data in our Privacy Policy.
+      </p>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <label>Full Name : </label>
+        <input {...register("fullName", { required: true })} defaultValue="" />
+        {errors.lastName && "full name is required"}
+        <label>Email : </label>
+        <input
+          type="email"
+          {...register("email", { required: true })}
+          defaultValue=""
+        />
+        {errors.lastName && "email is required"}
+        <label>Password : </label>
+        <input
+          type="password"
+          {...register("password", { required: true })}
+          defaultValue=""
+        />
+        {errors.lastName && "password is required"}
+        <label>Confirm Password : </label>
+        <input
+          type="password"
+          {...register("confirmPassword", { required: true })}
+          defaultValue=""
+        />
+        {errors.lastName && "confirm password is required"}
+        <input type="submit" />
+      </form>
     </div>
   );
 }
